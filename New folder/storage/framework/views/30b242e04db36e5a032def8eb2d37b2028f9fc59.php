1e1bffbadf7ded79dd21b2d9981c86be8a68c3b1
Prerna <dev5.apolloinfotech@gmail.com>
2025-10-08 05:29:59 +0530

    Delete New folder directory

--- New folder/storage/framework/views/30b242e04db36e5a032def8eb2d37b2028f9fc59.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH C:\wamp64\www\oro_veda\resources\views/common/admin/footer.blade.php ENDPATH**/ ?>
--- New folder/storage/framework/views/dd6271781061cec254e72a4ce9aed5e54a151ddb.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH /home4/sparsvss/sparsh/resources/views/common/admin/footer.blade.php ENDPATH**/ ?>
