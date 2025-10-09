3803ffd10995d0c986749666f4c628d900fc1070
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2025-10-09 17:35:27 +0530

    language wise price changes in admin and front

--- New folder/vendor/hpd/captcha/routes/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-Route::get('captcha/api/{config?}','\Hpd\Captcha\CaptchaController@getCaptchaApi')->middleware('web');
-Route::get('captcha/{config?}', '\Hpd\Captcha\CaptchaController@getCaptcha')->middleware('web');
